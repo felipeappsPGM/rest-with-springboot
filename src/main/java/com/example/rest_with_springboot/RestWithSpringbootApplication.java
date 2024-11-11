e03997c918f9ef68b322e1964fef4ee800edf912
felipeappsPGM <felypegt8@gmail.com>
2024-11-10 23:16:42 -0300

    first endpoint

--- src/main/java/com/example/rest_with_springboot/RestWithSpringbootApplication.java
+++ src/main/java/com/example/rest_with_springboot/RestWithSpringbootApplication.java
@@ -2,8 +2,10 @@ package com.example.rest_with_springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+
 public class RestWithSpringbootApplication {
 
 	public static void main(String[] args) {
