2ce2542a35fe1bb2229047822d927fc8c83fc8e8
felipeappsPGM <felypegt8@gmail.com>
2024-11-10 22:20:30 -0300

    first commit

---
+++ src/test/java/com/example/rest_with_springboot/RestWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_with_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
